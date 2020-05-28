4671adc705b3514f245a9dabd1768a51fa862434
Burgrokash <escamillagonzalo@gmail.com>
2020-05-27 21:22:42 -0300

    - Adding Lightmap shader. Also change project file structure.

---
+++ CubeCity/Assets/Scripts/Rendering/LightMapReplacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMapReplacer : MonoBehaviour
+{
+    [ContextMenu("Do")]
+    public void Replace()
+    {
+        Mesh meshToModify = GetComponent<MeshFilter>().sharedMesh;
+        Vector4 lightmapOffsetAndScale = GetComponent<Renderer>().lightmapScaleOffset;
+
+        Vector2[] modifiedUV2s = meshToModify.uv2;
+        for (int i = 0; i<meshToModify.uv2.Length; i++)
+        {
+            modifiedUV2s[i] = new Vector2(meshToModify.uv2[i].x* lightmapOffsetAndScale.x +
+            lightmapOffsetAndScale.z, meshToModify.uv2[i].y* lightmapOffsetAndScale.y +
+            lightmapOffsetAndScale.w);
+        }
+        meshToModify.uv2 = modifiedUV2s;
+    }
+}
+
