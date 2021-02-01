be2d232a872eebe60ca2f9d341829227c6c5a5ee
gasparinielias <gasparinielias@gmail.com>
2021-01-31 21:47:47 -0300

    Implement FacesDistribution which enables to specify the existing faces on a level

---
+++ CubeCity/Assets/Editor/FacesDistributionEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(DistributionItem))]
+public class DistributionItemDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Keyboard), label);
+        
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        var amountRect = new Rect(position.x, position.y, position.width, position.height);
+        EditorGUI.PropertyField(amountRect, property.FindPropertyRelative("amount"), GUIContent.none);
+
+        EditorGUI.EndProperty();
+    }
+}
