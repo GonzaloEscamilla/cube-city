57d2267f95ab441a74d050f52a7fe1bcedd54599
Burgrokash <escamillagonzalo@gmail.com>
2021-02-22 21:08:35 -0300

    - UI updates. Level end event added.

---
+++ CubeCity/Assets/Scripts/Data/GamePlayData/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Singleton<Player>
+{
+    private PlayerData _playerData;
+
+    public int StarsAmount
+    {
+        get
+        {
+            return PlayerPrefs.GetInt(PlayerData.StartsAmount.ToString());
+        }
+        set
+        {
+            PlayerPrefs.SetInt(PlayerData.StartsAmount.ToString(), value);
+        }
+    }
+
+    public float TimePlayed
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat(PlayerData.TimePlayed.ToString());
+        }
+        set
+        {
+            PlayerPrefs.SetFloat(PlayerData.TimePlayed.ToString(), value);
+        }
+    }
+
+    public int CristalsAmount
+    {
+        get
+        {
+            return PlayerPrefs.GetInt(PlayerData.CristalsAmount.ToString());
+        }
+        set
+        {
+            PlayerPrefs.SetInt(PlayerData.CristalsAmount.ToString(), value);
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        switch (_playerData)
+        {
+            case PlayerData.StartsAmount:
+                if (!PlayerPrefs.HasKey(PlayerData.StartsAmount.ToString()))
+                {
+                    PlayerPrefs.SetInt(PlayerData.StartsAmount.ToString(), 0);
+                }
+                break;
+            case PlayerData.CristalsAmount:
+                if (!PlayerPrefs.HasKey(PlayerData.CristalsAmount.ToString()))
+                {
+                    PlayerPrefs.SetInt(PlayerData.CristalsAmount.ToString(), 0);
+                }
+                break;
+            case PlayerData.TimePlayed:
+                if (!PlayerPrefs.HasKey(PlayerData.TimePlayed.ToString()))
+                {
+                    PlayerPrefs.SetFloat(PlayerData.TimePlayed.ToString(), 0);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    [ContextMenu("teST")]
+    public void Test()
+    {
+        Debug.Log(StarsAmount);
+        StarsAmount = 1;
+        Debug.Log(StarsAmount);
+    }
+}
+
+public enum PlayerData
+{
+    StartsAmount,
+    CristalsAmount,
+    TimePlayed
+}
---
+++ CubeCity/Assets/Scripts/UI/UILevelEnd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UILevelEnd : MonoBehaviour
+{
+    [SerializeField] private DOTweenAnimation fadePanel; 
+    [SerializeField] private DOTweenAnimation endLevelUI; 
+
+    private void Start() => Init();
+
+    private void Init()
+    {
+        EventsManager.control.onLevelEndEvent += OnLevelEnd;
+    }
+
+    private void OnLevelEnd(LevelEndData data)
+    {
+        fadePanel.DOPlayForward();
+        endLevelUI.DOPlayForward();
+    }
+}
