17bfc5ec32c98e9bed878e8dd30fd32290ea7645
Burgrokash <escamillagonzalo@gmail.com>
2021-02-27 21:43:35 -0300

    - Updates on optimization.

---
+++ CubeCity/Assets/Scripts/Debugg/Debugger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Debugger : MonoBehaviour
+{
+    [SerializeField] private Text txtTargetFramerate;
+    [SerializeField] private Text txtVSyncCount;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        Application.targetFrameRate = 30;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
+    {
+        txtTargetFramerate.text =  "TargetFrameRate: " + Screen.currentResolution.ToString();
+        txtVSyncCount.text = "VSyncCount: " + QualitySettings.vSyncCount.ToString();
+    }
+}
